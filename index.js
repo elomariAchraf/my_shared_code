dadf5be971e5ff84d381827d51954deb1fd4b932
achraf elomari <elomari.hraf@gmail.com>
2018-01-10 16:37:08 +0100

    setting index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var my_shared_code_ui = require('my_shared_code_ui.js')
+var my_shared_file_headless = require('my_shared_file_headless.js')
+
+module.exports = {
+  my_shared_code_ui: 'my_shared_code_ui',
+  my_shared_file_headless: 'my_shared_file_headless'
+}
